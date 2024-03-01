5ad8928fa3c0d4417be5294a893dfb6fe96fa0ba
uzairzia <uzair.governorsindh@gmail.com>
2024-03-01 12:16:44 +0500

    initial commit

---
+++ 7_Number_Eight.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(2 * 4);
+console.log(16 / 2);
+// let number1: number = 16;
+// let number2: number = 2;
+// console.log(number1 / number2);
+// console.log(number1 - number2);
+// console.log(number1 * number2);
+// console.log(number2 / number1);
